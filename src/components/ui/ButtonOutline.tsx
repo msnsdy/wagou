632b9834277b71a667105b3f690300feee8c850d
msnsdy <msnsdy0919@gmail.com>
2026-05-17 17:03:01 +0900

    アウトラインボタンコンポーネントのprops修正

--- src/components/ui/ButtonOutline.tsx
+++ src/components/ui/ButtonOutline.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 type ButtonOutlineProps = {
   href: string;
   text: string;
-  blank: boolean;
+  blank?: boolean;
 };
 
 export default function ButtonOutline({ href, text, blank = true }: ButtonOutlineProps) {
