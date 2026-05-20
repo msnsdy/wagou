b58067fe813db306f93f70c9e68ec43f7b3a8329
msnsdy <msnsdy0919@gmail.com>
2026-05-20 15:29:53 +0900

    メディア情報一覧ページの作成

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -2,6 +2,8 @@ import styles from "./Media.module.css";
 import PageFirstView from "@/components/layout/PageFirstView";
 import MediaItem from "@/components/ui/MediaItem";
 import Breadcrumb from "@/components/layout/Breadcrumb";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Media() {
   const medias = [
@@ -28,41 +30,116 @@ export default function Media() {
 
       <article className={styles.media}>
         <div className={styles.container}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-          </ul>
+          <div className={styles.wrapper}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+              <li className={styles.item}>
+                <MediaItem
+                  date="2025/10/10"
+                  title="雑誌〇〇に掲載されました。"
+                />
+              </li>
+            </ul>
+            <div className={styles.pagination}>
+              <Link className={`${styles.arrow} ${styles.prev}`} href="/blog/">
+                <Image
+                  src="/common/page-arrow.svg"
+                  alt=""
+                  width="9"
+                  height="13"
+                  className={styles.arrowImage}
+                />
+              </Link>
+              <div className={styles.numbers}>
+                <Link
+                  className={`${styles.number} ${styles.isCurrent}`}
+                  href="/blog/"
+                >
+                  1
+                </Link>
+                <Link className={styles.number} href="/blog/">
+                  2
+                </Link>
+                <Link className={styles.number} href="/blog/">
+                  3
+                </Link>
+                <Link className={styles.number} href="/blog/">
+                  4
+                </Link>
+                <Link className={styles.number} href="/blog/">
+                  5
+                </Link>
+              </div>
+              <Link className={`${styles.arrow} ${styles.next}`} href="/blog/">
+                <Image
+                  src="/common/page-arrow.svg"
+                  alt=""
+                  width="9"
+                  height="13"
+                  className={styles.arrowImage}
+                />
+              </Link>
+            </div>
+          </div>
           <Breadcrumb items={breadcrumbItems} />
         </div>
       </article>
