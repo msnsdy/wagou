04d1b9d3d6a541aff759bcafc1d646f0fa75db90
msnsdy <msnsdy0919@gmail.com>
2026-06-03 22:28:26 +0900

    トップページのメディア情報セクションとmicroCMSを連携

--- src/app/(home)/_components/MediaSection.tsx
+++ src/app/(home)/_components/MediaSection.tsx
@@ -6,7 +6,30 @@ import Image from "next/image";
 import MediaItem from "@/components/ui/MediaItem";
 import { formatDate } from "@/utils/date";
 
-export default function MediaSection() {
+import { client } from "@/app/lib/microcms";
+
+// メディア情報記事の型定義
+type Props = {
+  id: string;
+  title: string;
+  publishedAt: string;
+};
+
+// microCMSからブログ記事を取得
+async function getMediaPosts(): Promise<Props[]> {
+  const data = await client.get({
+    endpoint: "media", // 'media'はmicroCMSのエンドポイント名
+    queries: {
+      fields: "id,title,publishedAt", // idとtitleを取得
+      limit: 5, // 最新の5件を取得
+      orders: "-publishedAt",
+    },
+  });
+  return data.contents;
+}
+
+export default async function MediaSection() {
+  const posts = await getMediaPosts();
   return (
     <section className={styles.topMedia}>
       <div className={styles.bgWrap}>
@@ -24,24 +47,19 @@ export default function MediaSection() {
         </div>
         <div className={styles.content}>
           <ul className={styles.list}>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
-            <li className={styles.item}>
-              <MediaItem date="2025/10/10" title="雑誌〇〇に掲載されました。" />
-            </li>
+            {posts.map((post) => {
+              return (
+                <li className={styles.item} key={post.id}>
+                  <MediaItem
+                    date={formatDate(post.publishedAt)}
+                    title={post.title}
+                  />
+                </li>
+              );
+            })}
           </ul>
           <div className={styles.button}>
-            <Button href="/media" text="全てを見る" />
+            <Button href="/media/" text="全てを見る" />
           </div>
         </div>
         <div className={styles.illustWrap}>
